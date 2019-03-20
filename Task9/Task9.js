332c670af934e2794b036dfd8f535f176d5f863f
Sergey <amelin_serega@bk.ru>
2019-03-20 21:43:40 +0300

    lk

--- Task9/Task9.js
+++ Task9/Task9.js
@@ -1,19 +1,92 @@
+let a;
+let b;
+let operator;
+let result;
+ 
+
 function clc() {
-
- document.body.form.textview.value=""
-
+    a = 0;
+    b = 0;
+    operator = null;
+    result = null;
+    updateScreen () ;
 }
+
+
+
+
+
 function insert (num){
-    document.form.textview.value=document.form.textview.value+num
-}  
-function equal () {
-    let exp =   document.form.textview.value ;
-    if(exp){
-
-    document.form.textview.value = eval(exp)
+    if (!operator && !a) {
+        a = num;
+    } else if (!operator && a) {
+        a += num;
+    } else if (operator && !b) {
+        b = num;
+    } else if (operator && b) {
+        b += num;
     }
+
+    updateScreen();
 }
 
-/*Наполнять одну переменную которую ппреобразовывать в строку 
+function insertOperator(op){
+    if(result) {}
+    operator = op;
+    updateScreen();
+
+}
+
+function updateScreen() {
+    console.log('a', a);
+    console.log('operator', operator);
+    console.log('b', b);
+    if(!operator) {
+        document.querySelector('#textview').value = a || 0;
+    } else {
+        document.querySelector('#textview').value = b || 0;
+    }
+    
+
+    // document.querySelector('#textview').value = !operator ? a : b;
+}
+
+function equal () {
+   const numA = Number(a);
+   const numB= Number(b);
+   if (operator == '/' && b == 0){
+       return  ;
+   }
+    switch(operator)
+    {
+          case '-' :
+            a = numA-numB;
+            b = null ;
+            operator = null ;
+            break;
+          case '+' :
+          
+            a = numA + numB ;
+            b = null ;
+            operator = null ;
+            break;
+          case '/' :
+            a = numA/numB;
+            b = null ;
+            operator = null ;
+            break;
+          case '*' :
+            a = numA * numB ;
+            b = null ;
+            operator = null ;
+            break 
+    }
+        a = a.toString() ;
+  
+    updateScreen()
+    
+}
+
+/*Наполнять одну переменную которую преобразовывать в строку 
 и затем производить с ней вычисления
 */ 
